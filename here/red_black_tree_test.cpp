d511b68f078c5e59556983cb74056742bb11d5b1
Илья Горьков <144900623+Ilreddem@users.noreply.github.com>
2025-06-06 00:02:11 +0300

    Update red_black_tree_test.cpp

--- here/red_black_tree_test.cpp
+++ here/red_black_tree_test.cpp
@@ -19,8 +19,6 @@ TEST(RedBlackTreeTest, InsertSingleNode) {
     EXPECT_EQ(tree.root->data, 25);
     EXPECT_EQ(tree.root->color, BLACK);
 }
-
-// Тест на правильный цвет детей после вставки нескольких узлов
 TEST(RedBlackTreeTest, InsertMultipleNodesColorsCheck) {
     RedBlackTree tree;
     tree.insert(10);
@@ -35,7 +33,6 @@ TEST(RedBlackTreeTest, InsertMultipleNodesColorsCheck) {
     EXPECT_EQ(tree.root->right->color, RED);
 }
 
-// Тест на обход дерева (inorder должен выдавать отсортированную последовательность)
 TEST(RedBlackTreeTest, InorderTraversalOutputsSorted) {
     RedBlackTree tree;
     std::stringstream buffer;
@@ -56,7 +53,98 @@ TEST(RedBlackTreeTest, InorderTraversalOutputsSorted) {
     std::string output = buffer.str();
     EXPECT_EQ(output, "20 30 40 50 60 70 80 ");
 }
-enable_testing()
-add_executable(RBTreeTests red_black_tree_test.cpp RedBlackTree.cpp)
-target_link_libraries(RBTreeTests gtest gtest_main)
-add_test(NAME RBTreeTests COMMAND RBTreeTests)
+TEST(RedBlackTreeTest, InsertLeftRight) {
+    RedBlackTree tree;
+    tree.insert(20);
+    tree.insert(10);
+    tree.insert(15);
+    EXPECT_EQ(tree.root->left->right->data, 15);
+}
+
+TEST(RedBlackTreeTest, InsertRightLeft) {
+    RedBlackTree tree;
+    tree.insert(20);
+    tree.insert(30);
+    tree.insert(25);
+    EXPECT_EQ(tree.root->right->left->data, 25);
+}
+
+TEST(RedBlackTreeTest, RootAlwaysBlack) {
+    RedBlackTree tree;
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(1);
+    EXPECT_EQ(tree.root->color, BLACK);
+}
+
+TEST(RedBlackTreeTest, LeftChildLessThanParent) {
+    RedBlackTree tree;
+    tree.insert(20);
+    tree.insert(10);
+    EXPECT_LT(tree.root->left->data, tree.root->data);
+}
+
+TEST(RedBlackTreeTest, RightChildGreaterThanParent) {
+    RedBlackTree tree;
+    tree.insert(20);
+    tree.insert(30);
+    EXPECT_GT(tree.root->right->data, tree.root->data);
+}
+
+TEST(RedBlackTreeTest, DoubleRotationLeftRight) {
+    RedBlackTree tree;
+    tree.insert(30);
+    tree.insert(10);
+    tree.insert(20);
+    EXPECT_EQ(tree.root->data, 20);
+}
+
+TEST(RedBlackTreeTest, DoubleRotationRightLeft) {
+    RedBlackTree tree;
+    tree.insert(10);
+    tree.insert(30);
+    tree.insert(20);
+    EXPECT_EQ(tree.root->data, 20);
+}
+
+TEST(RedBlackTreeTest, TreeHandlesDuplicateInsertion) {
+    RedBlackTree tree;
+    tree.insert(10);
+    tree.insert(10); // дубликат, но у тебя пока нет запрета
+    EXPECT_EQ(tree.root->data, 10);
+}
+
+TEST(RedBlackTreeTest, TreeInorderCorrectness) {
+    RedBlackTree tree;
+    std::stringstream buffer;
+    std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+
+    for (int v : {40, 20, 60, 10, 30, 50, 70}) {
+        tree.insert(v);
+    }
+    tree.inorder();
+
+    std::cout.rdbuf(old);
+    EXPECT_EQ(buffer.str(), "10 20 30 40 50 60 70 ");
+}
+
+TEST(RedBlackTreeTest, LeftRightStructureTest) {
+    RedBlackTree tree;
+    tree.insert(10);
+    tree.insert(5);
+    tree.insert(1);
+    EXPECT_TRUE(tree.root->left != nullptr);
+    EXPECT_TRUE(tree.root->left->left != nullptr);
+}
+
+TEST(RedBlackTreeTest, DeepInsertionsAndStructure) {
+    RedBlackTree tree;
+    for (int i = 1; i <= 15; ++i)
+        tree.insert(i);
+
+    EXPECT_EQ(tree.root->color, BLACK);
+    EXPECT_TRUE(tree.root->left != nullptr);
+    EXPECT_TRUE(tree.root->right != nullptr);
+}
+
